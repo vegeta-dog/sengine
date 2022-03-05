02a57103222622393802a268243bbc485213754e
fslongjin <fslongjin@vip.qq.com>
2022-03-06 01:02:26 +0800

    :new: 引入了EasyLogger模块

--- src/utils/configParser/configParser.cpp
+++ src/utils/configParser/configParser.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "configParser.h"
+#include<cstdio>
+#include<cstdlib>
+#include<stdexcept>
+#include<iostream>
+
+int configParser::parse(const std::string& path, std::map<std::string, std::string> *conf_str, std::map<std::string, int> *conf_int,
+          std::map<std::string, double> *conf_double)
+{
+    FILE *fp;
+    if((fp = fopen(path.c_str(), "r+"))== nullptr)
+    {
+        return PATH_ERROR;
+    }
+    try {
+
+    }
+    catch (std::exception e)
+    {
+        printf("");
+    }
+    return SUCCESS;
+};
--- src/utils/configParser/configParser.h
+++ src/utils/configParser/configParser.h
@@ -5,12 +5,31 @@
 #ifndef SENGINE_CONFIGPARSER_H
 #define SENGINE_CONFIGPARSER_H
 
+#include<map>
 
-namespace configParser
-{
-    class configParser {
+/**
+ * 定义错误码
+ */
+// 执行成功
+#define SUCCESS 0
+// 文件路径错误
+#define PATH_ERROR 1
+// 配置文件格式错误
+#define SYNTAX_ERROR
 
-    };
+
+namespace configParser {
+
+
+    /** 解析配置文件
+     * @param path 文件路径
+     * @param conf_str string类型的参数
+     * @param conf_int int类型的参数
+     * @param conf_double 浮点类型的参数
+     * @return
+     */
+    int parse(const std::string& path, std::map<std::string, std::string> *conf_str, std::map<std::string, int> *conf_int,
+              std::map<std::string, double> *conf_double);
 }
 
 
