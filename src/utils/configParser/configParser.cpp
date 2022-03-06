94fcb31238696143322535437d326fee0ca529c0
fslongjin <fslongjin@vip.qq.com>
2022-03-06 11:15:00 +0800

    :new: 初步完成了configParser

--- src/utils/configParser/configParser.cpp
+++ src/utils/configParser/configParser.cpp
@@ -7,21 +7,52 @@
 #include<cstdlib>
 #include<stdexcept>
 #include<iostream>
+#include<boost/property_tree/ptree.hpp>
 
-int configParser::parse(const std::string& path, std::map<std::string, std::string> *conf_str, std::map<std::string, int> *conf_int,
-          std::map<std::string, double> *conf_double)
-{
-    FILE *fp;
-    if((fp = fopen(path.c_str(), "r+"))== nullptr)
-    {
-        return PATH_ERROR;
-    }
+
+#include<boost/property_tree/ini_parser.hpp>
+
+boost::property_tree::ptree config;
+#define GET_SECTION(str)                    \
+(                                           \
+    str.substr(0, str.find('.'))   \
+)
+
+/** 解析配置文件
+ * @param path 文件路径
+ * @param conf_str string类型的参数
+ * @param conf_int int类型的参数
+ * @param conf_double 浮点类型的参数
+ * @return
+ */
+int configParser::parse(const std::string &path) {
     try {
 
+        boost::property_tree::ini_parser::read_ini(path, config);
     }
-    catch (std::exception e)
-    {
-        printf("");
+    catch (const std::runtime_error &e) {
+        printf("%s", e.what());
+
     }
     return SUCCESS;
 };
+/**
+ * 获取配置项
+ * @param key 关键字，格式为 <section, key_name>
+ * @param data 返回数据的指针
+ */
+
+int configParser::get_config(const std::string &key, void *data) {
+
+    std::string type = GET_SECTION(key);
+
+    if (type == "int")
+        *(int *) data = config.get<int>(key);
+    else if (type == "double")
+        *(double *) data = config.get<double>(key);
+    else if (type == "float")
+        *(float *) data = config.get<float>(key);
+    else
+        *(std::string *) data = config.get<std::string>(key);
+    return SUCCESS;
+}
--- src/utils/configParser/configParser.h
+++ src/utils/configParser/configParser.h
@@ -6,30 +6,39 @@
 #define SENGINE_CONFIGPARSER_H
 
 #include<map>
-
+#include<boost/property_tree/ptree.hpp>
+#include<boost/property_tree/ini_parser.hpp>
 /**
  * 定义错误码
  */
 // 执行成功
 #define SUCCESS 0
 // 文件路径错误
-#define PATH_ERROR 1
+#define E_PATH_ERROR 1
 // 配置文件格式错误
-#define SYNTAX_ERROR
+#define E_SYNTAX_ERROR
+
 
 
 namespace configParser {
 
 
-    /** 解析配置文件
-     * @param path 文件路径
-     * @param conf_str string类型的参数
-     * @param conf_int int类型的参数
-     * @param conf_double 浮点类型的参数
-     * @return
-     */
-    int parse(const std::string& path, std::map<std::string, std::string> *conf_str, std::map<std::string, int> *conf_int,
-              std::map<std::string, double> *conf_double);
+/** 解析配置文件
+ * @param path 文件路径
+ * @param conf_str string类型的参数
+ * @param conf_int int类型的参数
+ * @param conf_double 浮点类型的参数
+ * @return
+ */
+    int parse(const std::string &path);
+
+/**
+ * 获取配置项
+ * @param key 关键字，格式为 <section, key_name>
+ * @param data 返回数据的指针
+ */
+    int get_config(const std::string &key, void* data);
+
 }
 
 
