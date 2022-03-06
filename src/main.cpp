cee0f44ee005f5b49e96f80f93a187a56b41aa27
fslongjin <fslongjin@vip.qq.com>
2022-03-06 11:16:33 +0800

    :heavy_minus_sign: 移除EasyLogger

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include<database/DataBase.h>
+#include<utils/configParser/configParser.h>
+#include<map>
 
 int main() {
 
     Database::example();
+
+    configParser::parse("config.ini");
+    std::string data;
+    configParser::get_config("string.a", &data);
+    std::cout<<data<<std::endl;
     return 0;
 }
