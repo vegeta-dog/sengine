2758f78d60f70c4a864cec7d801f0fa3e19a807e
fslongjin <fslongjin@vip.qq.com>
2022-05-23 14:06:31 +0800

    :new:截取含有中文的字符串

---
+++ src/utils/utf/utf.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+namespace libUTF
+{
+    /**
+     * @brief 截取含有utf字符的字符串
+     * 
+     * @param begin 起始位置
+     * @param len 结束位置
+     * @return std::string 结果串 
+     */
+    std::string substr_utf(const std::string & data, int begin, int len);
+}
